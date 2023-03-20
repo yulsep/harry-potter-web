cc0694c87fe689f1226402a3c9f40cf576d8be12
yulsep <yulimarcela99@gmail.com>
2023-03-20 16:13:27 -0500

    services

--- src/services/houses.service.js
+++ src/services/houses.service.js
@@ -1,14 +1,13 @@
-// import { API } from "../api";
-// import { houseMock } from "../mock";
-// import { BASE_URL } from "../utils";
+import { API } from "../api";
+import { houseMock } from "../mock";
+import { BASE_URL } from "../utils";
 
-// export async function fetchHousesByName(houseName = "") {
-//   try {
-//     //const houseData = await API.get(`${BASE_URL}characters/house/${houseName}`);
-//     const houseData = await houseMock(houseName);
-//     return houseData;
-//   } catch (error) {
-//     throw new Error(`Problems to get ${houseName} data...`);
-//   }
-// }
-//
+export async function fetchHousesByName(houseName = "") {
+  try {
+    //const houseData = await API.get(`${BASE_URL}characters/house/${houseName}`);
+    const houseData = await houseMock(houseName);
+    return houseData;
+  } catch (error) {
+    throw new Error(`Problems to get ${houseName} data...`);
+  }
+}
